603b5e0bc9ac281b9ea4017ca50cdcb85e6fa905
Sivagnanasuntharam piriyaraj <piriyaraj1998@gmail.com>
2023-03-30 19:38:24 +0530

    updated

--- Run.cmd
+++ Run.cmd
@@ -3,7 +3,7 @@ if not exist venv (
     python -m venv venv
 )
 call venv\Scripts\activate
-@REM git add .
+git restore .> log.txt 2>&1
 git pull> log.txt 2>&1
 @echo off
 pip install -r requirements.txt > log.txt 2>&1
