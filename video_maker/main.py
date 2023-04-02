184f9c20704406499b0e528de4e81675cd33a639
Sivagnanasuntharam piriyaraj <piriyaraj1998@gmail.com>
2023-04-02 22:41:59 +0530

    updated

--- video_maker/main.py
+++ video_maker/main.py
@@ -7,27 +7,27 @@ from usecases.record_video import RecordVideo
 from usecases.upload_youtube import UploadYoutube
 import os
 
-if os.path.exists("media/replays"):
+if not os.path.exists("media/replays"):
     os.makedirs("media/replays")
-if os.path.exists("media/thumb"):
+if not os.path.exists("media/thumb"):
     os.makedirs("media/thumb")
-if os.path.exists("media/uploaded"):
+if not os.path.exists("media/uploaded"):
     os.makedirs("media/uploaded")
-if os.path.exists("media/Videos"):
+if not os.path.exists("media/Videos"):
     os.makedirs("media/Videos")
 try:
-    # lol_data_scrapper = ScrapLolData()
-    # lol_data_scrapper.get_match_data_and_download_replay()
+    lol_data_scrapper = ScrapLolData()
+    lol_data_scrapper.get_match_data_and_download_replay()
     lol_data: MatchData = load()
     thumb_creator = CreateThumbnail(data_scrapper=DataScrapper(), data=lol_data)
     thumb_creator.create_thumbnail()
 
-    # video_recorder = RecordVideo(lol_data)
-    # video_file_name = video_recorder.record()
-    # # video_file_name = video_recorder.select_video_file()
+    video_recorder = RecordVideo(lol_data)
+    video_file_name = video_recorder.record()
+    # video_file_name = video_recorder.select_video_file()
 
-    # youtube_uploader = UploadYoutube(lol_data, video_file_name)
-    # youtube_uploader.upload_video()
-    # video_recorder.remove_video_file()
+    youtube_uploader = UploadYoutube(lol_data, video_file_name)
+    youtube_uploader.upload_video()
+    video_recorder.remove_video_file()
 except Exception as e:
     print(e)
