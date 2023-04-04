2a28e701b36b4325e39f23d11142c736654bb96c
Sivagnanasuntharam piriyaraj <piriyaraj1998@gmail.com>
2023-04-04 21:21:08 +0530

    updated

--- video_maker/usecases/scrap_lol_data.py
+++ video_maker/usecases/scrap_lol_data.py
@@ -290,12 +290,16 @@ class ScrapLolData(DataScrapper):
             for player in match_data['team1']['players']:
                 team = 'team1'
                 loser_team = 'team2'
-                kdas.append(int(player['kda'].split(' ')[0]))
+                values=player['kda'].split(" / ")
+                kdaValue=(int(values[0])+int(values[1]))/int(values[2])
+                kdas.append(kdaValue)
         else:
             for player in match_data['team2']['players']:
                 team = 'team2'
                 loser_team = 'team1'
-                kdas.append(int(player['kda'].split(' ')[0]))
+                values=player['kda'].split(" / ")
+                kdaValue=(int(values[0])+int(values[1]))/int(values[2])
+                kdas.append(kdaValue)
         player_index = kdas.index(max(kdas))
         roles = ['Top', 'Jungle', 'Mid', 'ADC', 'Support']
         return {
